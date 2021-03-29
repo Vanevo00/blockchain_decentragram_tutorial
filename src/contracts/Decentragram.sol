e6a3dd04e9eccc3e3b2eb3f78eb586dec6691348
Vanek <vanevo00@gmail.com>
2021-03-29 20:15:40 +0200

    feat: migrations, start uploadImage function

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,19 @@
 pragma solidity ^0.5.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    mapping(uint => Image) public images;
+
+    struct Image {
+        uint id;
+        string hash;
+        string description;
+        uint tipAmout;
+        address payable author;
+    }
+
+    function uploadImage() public {
+        images[1] = Image(1, 'abs', 'pes', 0, address(0x0));
+    }
 }
