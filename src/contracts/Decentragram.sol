96b8664e61b69568abbed5b84031e6193093bd8c
Vanek <vanevo00@gmail.com>
2021-03-31 17:46:28 +0200

    feat: uploadImage function and tests

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.5.0;
 contract Decentragram {
     string public name = "Decentragram";
 
+    uint public imageCount = 0;
     mapping(uint => Image) public images;
 
     struct Image {
@@ -13,7 +14,21 @@ contract Decentragram {
         address payable author;
     }
 
-    function uploadImage() public {
-        images[1] = Image(1, 'abs', 'pes', 0, address(0x0));
+    event ImageCreated (
+        uint id,
+        string hash,
+        string description,
+        uint tipAmount,
+        address payable author
+    );
+
+    function uploadImage(string memory _imgHash, string memory _description) public {
+        require(bytes(_description).length > 0);
+        require(bytes(_imgHash).length > 0);
+        require(msg.sender != address(0x0));
+
+        imageCount ++;
+        images[imageCount] = Image(imageCount, _imgHash, _description, 0, msg.sender);
+        emit ImageCreated(imageCount, _imgHash, _description, 0, msg.sender);
     }
 }
