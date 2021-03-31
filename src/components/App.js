dd5ec45a221b62322561f517a8a5c0d74b462f36
Vanek <vanevo00@gmail.com>
2021-03-31 20:34:08 +0200

    feat: load web3 and load basic account data

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,48 @@ import Main from './Main'
 
 
 class App extends Component {
-
   constructor(props) {
     super(props)
     this.state = {
       account: '',
     }
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockChainData() {
+    const web3 = window.web3
+
+    const accounts = await web3.eth.getAccounts()
+    this.setState({
+      account: accounts[0]
+    })
+
+    const networkId = await web3.eth.net.getId()
+    const networkData = Decentragram.networks[networkId]
+
+    if (networkData) {
+      const decentragram = web3.eth.Contract(Decentragram.abi, networkData.address)
+    } else {
+      window.alert(`Decentragram not deployed on the selected network (id: ${networkId})`)
+    }
+
+  }
+
+  async componentDidMount() {
+    await this.loadWeb3()
+    await this.loadBlockChainData()
   }
 
   render() {
